0cb783a380292503df0764564aa0d84870375f5c
googolmo <googolmo@gmail.com>
2012-03-08 22:03:46 +0800

    update tree

---
+++ src/com/googolmo/foursquare/utils/Configuration.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.googolmo.foursquare.utils;
+
+/**
+ * @author googolmo
+ *
+ */
+public class Configuration {
+
+	/**
+	 * 
+	 */
+	public Configuration() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
