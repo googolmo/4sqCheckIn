e0979ebf644045f3e11a457b2afbf0d1a474a8dc
googolmo <googolmo@gmail.com>
2012-03-08 21:57:02 +0800

    First commit

---
+++ src/com/googolmo/foursquare/http/HttpClient.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.googolmo.foursquare.http;
+
+/**
+ * @author googolmo
+ *
+ */
+public class HttpClient {
+
+	/**
+	 * 
+	 */
+	public HttpClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
